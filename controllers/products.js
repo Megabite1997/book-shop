c4c69ea20c7f1662d48e9fd1ce896704ecb1c1f1
Tada Chaipanya <fendersdtua@hotmail.com>
2024-03-03 20:15:09 +0700

    Add models

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
-const products = [];
+// const products = [];
+const Product = require("../models/products");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -11,11 +12,14 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  // products.push({ title: req.body.title }); // Use models instead, below
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render("shop", {
     prods: products,
     pageTitle: "Shop",
